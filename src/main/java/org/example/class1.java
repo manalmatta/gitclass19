fb00b014ece71b03cae483f89ec8b952ff5b4cb0
manal <manalmatta706@gmail.com>
2024-03-13 20:33:46 -0400

    second commit

--- src/main/java/org/example/class1.java
+++ src/main/java/org/example/class1.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("Hellow world");
+        System.out.println("Hello world");
+        System.out.println("Hello marsg");
     }
 }
